15d4876a2366be791b5798a9f7b6e92db68e0396
Marcio Ponciano <mponcianos@gmail.com>
2019-10-14 11:34:25 -0300

    Novo material

--- aula_09/aula09_03_requests.py
+++
@@ -1,9 +0,0 @@
-# CONSUMINDO API
-import requests
-import json
-ler_cep = input('Informe o CEP: ')
-
-req = requests.get("https://viacep.com.br/ws/{}/json/".format(ler_cep))
-
-print(req.json())
-json.loads(req.text)
